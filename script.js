0f761e45116201d9a54f32449a2348c81c1dc889
alihernandez <ali.hernandez9two@gmail.com>
2023-04-23 16:51:32 -0600

    JS validation

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm() {
+    //Using Constraint API
+    isValid = form.checkValidity();
+    // console.log(isValid);
+    
+    //Style main error message
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+}
+
+function processFormData(e){
+    e.preventDefault();
+    // console.log(e);
+    validateForm();
+}
+
+//Event Listener
+form.addEventListener('submit', processFormData);
